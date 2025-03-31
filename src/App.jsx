518aa893b12bc97379e2dc49cdad1a736290a8c4
Udara Lakshan <ulakshan061@gmail.com>
2025-03-31 22:38:20 +0530

    refactor App component to include Header and RequestAmountPage, comment out unused code

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-function App() {
+// function App() {
+//   return (
+//     <div>
+//       <h1 className="text-red-500">This is Loan tube 24</h1>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import Header from "./components/Header"
+import LoanPurposePage from "./pages/LoanPurposePage"
+import RequestAmountPage from "./pages/RequestAmountPage"
+
+const App = () => {
   return (
-    <div>
-      <h1 className="text-red-500">This is Loan tube 24</h1>
+    <div className="mx-auto flex max-w-screen-xl flex-col items-center">
+      <Header />
+      {/* <LoanPurposePage /> */}
+      <RequestAmountPage />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
