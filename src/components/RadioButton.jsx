b311fe141459d33acfd1ae047a84af682f7c9a29
Udara Lakshan <ulakshan061@gmail.com>
2025-03-31 22:39:45 +0530

    style: improve RadioButton component styling for better user interaction

--- src/components/RadioButton.jsx
+++ src/components/RadioButton.jsx
@@ -1,20 +1,20 @@
 const RadioButton = ({ item, selected, handleItemChange }) => {
   return (
     <div
-      className={`flex items-center rounded-sm border-2 px-4 ${selected === item ? "border-blue-400 bg-blue-50" : "border-gray-200"}`}
+      className={`flex cursor-pointer items-center rounded-sm border-2 px-4 ${selected === item ? "border-blue-600 bg-blue-100" : "border-gray-200"}`}
     >
       <input
         id={item}
         type="radio"
         value={item}
         name="bordered-radio"
-        className="h-4 w-4 border-gray-300 bg-gray-100 text-blue-600"
+        className="h-5 w-5 border-gray-300 bg-gray-100 text-blue-600"
         checked={selected === item}
         onChange={(e) => handleItemChange(e.target.value)}
       />
       <label
         htmlFor={item}
-        className="ms-2 w-full py-4 text-sm font-medium text-gray-900"
+        className="mx-4 w-full py-4 text-sm font-semibold text-gray-900 md:text-base"
       >
         {item}
       </label>
