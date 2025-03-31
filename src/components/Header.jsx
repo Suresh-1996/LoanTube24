148078672f1f8dc898cc2769de5f3fe41998de8d
Udara Lakshan <ulakshan061@gmail.com>
2025-03-31 22:39:25 +0530

    add Header component with logo and styling

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Header = () => {
+  return (
+    <header className="flex h-15 w-screen justify-center shadow-md">
+      <img
+        src="https://getlogovector.com/wp-content/uploads/2021/08/nerdwallet-logo-vector.png"
+        className="w-40 object-cover"
+      />
+    </header>
+  )
+}
+
+export default Header
