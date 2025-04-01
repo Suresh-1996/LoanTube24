6aafabb1915ada1e610b72f7e8573ade5e5522a4
Udara Lakshan <ulakshan061@gmail.com>
2025-04-01 12:46:41 +0530

    style: update layout and styles in RequestAmountPage for improved visual hierarchy

--- src/pages/RequestAmountPage.jsx
+++ src/pages/RequestAmountPage.jsx
@@ -11,12 +11,12 @@ const RequestAmountPage = () => {
 
   return (
     <div className="w-[90%] md:w-[60%] lg:w-[50%]">
-      <div className="mt-10 flex w-full flex-col items-center">
-        <h1 className="text-center font-serif text-3xl font-medium md:text-4xl">
+      <div className="mt-16 flex w-full flex-col items-center">
+        <h1 className="text-center font-serif text-3xl font-medium md:text-5xl">
           What's your estimated <br /> debt balance?
         </h1>
-        <div className="relative my-10 flex w-full flex-col items-center gap-3">
-          <h2 className="mb-4 text-4xl text-green-800">
+        <div className="relative my-12 flex w-full flex-col items-center gap-3">
+          <h2 className="mb-4 text-[2.5rem] text-[#008254]">
             ${value.toLocaleString()}
           </h2>
           <input
@@ -27,18 +27,14 @@ const RequestAmountPage = () => {
             value={value}
             onChange={(e) => setValue(Number(e.target.value))}
             style={{
-              background: `linear-gradient(to right, #16A34A ${progress}%, #E5E7EB ${progress}%)`,
+              background: `linear-gradient(to right, #008254 ${progress}%, #E5E7EB ${progress}%)`,
             }}
             className="h-3 w-[90%] cursor-pointer appearance-none rounded-lg md:w-[95%]"
           />
-          <span className="absolute start-0 -bottom-9 font-medium text-gray-500">
-            $2,000
-          </span>
-          <span className="absolute end-0 -bottom-9 font-medium text-gray-500">
-            $100,000
-          </span>
+          <span className="absolute start-0 -bottom-9">$2,000</span>
+          <span className="absolute end-0 -bottom-9">$100,000</span>
         </div>
-        <button className="mt-5 w-full cursor-pointer rounded-sm bg-green-800 py-3 text-sm font-bold text-white">
+        <button className="mt-7 w-full cursor-pointer rounded-sm bg-[#006642] py-3 text-sm font-bold text-white">
           CONTINUE
         </button>
       </div>
