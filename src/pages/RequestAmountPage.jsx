f676e7b815a2f5d0730a19af51a2bcf842353173
Udara Lakshan <ulakshan061@gmail.com>
2025-04-01 16:02:55 +0530

    style: enhance layout and styling in RequestAmountPage for improved responsiveness and visual appeal

--- src/pages/RequestAmountPage.jsx
+++ src/pages/RequestAmountPage.jsx
@@ -10,31 +10,37 @@ const RequestAmountPage = () => {
   const progress = ((value - min) / (max - min)) * 100
 
   return (
-    <div className="w-[90%] md:w-[60%] lg:w-[50%]">
-      <div className="mt-16 flex w-full flex-col items-center">
-        <h1 className="text-center font-serif text-3xl font-medium md:text-5xl">
+    <div className="w-screen px-4 md:px-16 lg:px-48 xl:px-100 2xl:px-160">
+      <div className="flex w-full flex-col items-center py-8 lg:py-16">
+        <h1 className="text-center font-serif text-[34px] leading-10 font-medium md:text-[44px] md:leading-14">
           What's your estimated <br /> debt balance?
         </h1>
-        <div className="relative my-12 flex w-full flex-col items-center gap-3">
+        <div className="relative mt-8 flex w-full flex-col items-center gap-3 lg:mt-12">
           <h2 className="mb-4 text-[2.5rem] text-[#008254]">
             ${value.toLocaleString()}
           </h2>
-          <input
-            type="range"
-            min={min}
-            max={max}
-            step={100}
-            value={value}
-            onChange={(e) => setValue(Number(e.target.value))}
-            style={{
-              background: `linear-gradient(to right, #008254 ${progress}%, #E5E7EB ${progress}%)`,
-            }}
-            className="h-3 w-[90%] cursor-pointer appearance-none rounded-lg md:w-[95%]"
-          />
-          <span className="absolute start-0 -bottom-9">$2,000</span>
-          <span className="absolute end-0 -bottom-9">$100,000</span>
+          <div className="w-full px-4">
+            <input
+              type="range"
+              min={min}
+              max={max}
+              step={100}
+              value={value}
+              onChange={(e) => setValue(Number(e.target.value))}
+              style={{
+                background: `linear-gradient(to right, #008254 ${progress}%, #E5E7EB ${progress}%)`,
+              }}
+              className="h-4 w-full cursor-pointer appearance-none rounded-lg"
+            />
+          </div>
+          <span className="absolute start-0 -bottom-9 text-sm font-medium md:text-base">
+            $2,000
+          </span>
+          <span className="absolute end-0 -bottom-9 text-sm font-medium md:text-base">
+            $100,000
+          </span>
         </div>
-        <button className="mt-7 w-full cursor-pointer rounded-sm bg-[#006642] py-3 text-sm font-bold text-white">
+        <button className="mt-15 w-full cursor-pointer rounded-md bg-[#006642] p-4 text-sm font-bold text-white">
           CONTINUE
         </button>
       </div>
