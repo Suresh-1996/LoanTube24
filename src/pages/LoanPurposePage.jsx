e1abc066c49aea2fc196969fef9f9b87c75cbc70
Udara Lakshan <ulakshan061@gmail.com>
2025-04-01 15:41:37 +0530

    style: update LoanPurposePage layout and add new loan purposes for improved user experience

--- src/pages/LoanPurposePage.jsx
+++ src/pages/LoanPurposePage.jsx
@@ -21,14 +21,16 @@ const LoanPurposePage = () => {
     "Education",
     "Vacation",
     "Wedding",
+    "IRS tax debt",
+    "Other",
   ]
 
   return (
-    <div className="my-8 flex w-full flex-col items-center md:my-10">
-      <h1 className="text-center font-serif text-[34px] leading-10 font-medium md:w-[70%] md:text-[44px] md:leading-13 lg:w-[50%]">
+    <div className="flex w-full flex-col items-center px-4 py-8 md:px-16 lg:px-48 lg:py-16 xl:px-100 2xl:px-160">
+      <h1 className="text-center font-serif text-[34px] leading-10 font-medium md:w-[80%] md:text-[44px] md:leading-13 xl:w-[90%]">
         How are you going to use the money?
       </h1>
-      <div className="mt-8 flex w-[90%] flex-col gap-3 md:my-10 md:w-[85%] lg:w-[65%]">
+      <div className="mt-8 flex w-full flex-col gap-3 lg:mt-10">
         {purposes.map((purpose, index) => (
           <RadioButton
             title={purpose}
