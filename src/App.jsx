bc5466f0b175571c1506d2f1a1389ccd3f9289c2
Udara Lakshan <ulakshan061@gmail.com>
2025-04-01 20:31:04 +0530

    reset to default App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,9 @@
-// function App() {
-//   return (
-//     <div>
-//       <h1 className="text-red-500">This is Loan tube 24</h1>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-import Header from "./components/Header"
-import LoanPurposePage from "./pages/LoanPurposePage"
-import RequestAmountPage from "./pages/RequestAmountPage"
-
-const App = () => {
+function App() {
   return (
-    <div className="mx-auto flex max-w-screen-xl flex-col items-center">
-      <Header />
-      {/* <LoanPurposePage /> */}
-      <RequestAmountPage />
+    <div>
+      <h1 className="text-red-500">This is Loan tube 24</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
