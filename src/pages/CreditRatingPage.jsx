b527f4f8de49b1e4458614b919cccf9891f120d6
Udara Lakshan <ulakshan061@gmail.com>
2025-04-02 15:58:26 +0530

    feat: update CreditRatingPage styling with Playfair Display font and adjust padding

--- src/pages/CreditRatingPage.jsx
+++ src/pages/CreditRatingPage.jsx
@@ -16,8 +16,8 @@ const CreditRatingPage = () => {
   ]
 
   return (
-    <div className="flex w-full flex-col items-center px-4 py-8 md:px-16 lg:px-48 lg:py-16 xl:px-100 2xl:px-160">
-      <h1 className="text-center font-serif text-[34px] leading-10 font-medium md:text-[44px] md:leading-13">
+    <div className="flex w-full flex-col items-center px-4 py-8 md:px-14 lg:px-46 lg:py-16 xl:px-98 2xl:px-158">
+      <h1 className="font-playfair text-center text-[34px] leading-10 font-semibold md:text-[44px] md:leading-13">
         How would you rate your credit?
       </h1>
       <div className="mt-8 flex w-full flex-col gap-3 md:mt-12">
