c78b1b8c7fb5cbbc7f57e7a277e95869d3bd54bf
Udara Lakshan <ulakshan061@gmail.com>
2025-04-02 15:58:42 +0530

    feat: update GreetingPage styling with Playfair Display font and adjust font weight

--- src/pages/GreetingPage.jsx
+++ src/pages/GreetingPage.jsx
@@ -4,7 +4,7 @@ const GreetingPage = () => {
   return (
     <div className="w-screen px-4 md:px-16 lg:px-48 xl:px-100 2xl:px-160">
       <div className="flex w-full flex-col items-center py-8 lg:py-16">
-        <h1 className="text-center font-serif text-[34px] leading-10 font-medium md:text-[44px] md:leading-14">
+        <h1 className="font-playfair text-center text-[34px] leading-10 font-semibold md:text-[44px] md:leading-14">
           Thank you for submitting. <br />
           Our lending officer will contact you to assist with your request.
         </h1>
