a9450cddc9e881dd8aac18464edfa69a683d9ad2
Udara Lakshan <ulakshan061@gmail.com>
2025-04-02 15:58:53 +0530

    feat: update LoanPurposePage styling with Playfair Display font and adjust font weight and padding

--- src/pages/LoanPurposePage.jsx
+++ src/pages/LoanPurposePage.jsx
@@ -26,8 +26,8 @@ const LoanPurposePage = () => {
   ]
 
   return (
-    <div className="flex w-full flex-col items-center px-4 py-8 md:px-16 lg:px-48 lg:py-16 xl:px-100 2xl:px-160">
-      <h1 className="text-center font-serif text-[34px] leading-10 font-medium md:w-[80%] md:text-[44px] md:leading-13 xl:w-[90%]">
+    <div className="flex w-full flex-col items-center px-4 py-8 md:px-16 lg:px-48 lg:py-16 xl:px-101 2xl:px-161">
+      <h1 className="font-playfair text-center text-[34px] leading-10 font-semibold md:w-[80%] md:text-[44px] md:leading-13 xl:w-[90%]">
         How are you going to use the money?
       </h1>
       <div className="mt-8 flex w-full flex-col gap-3 lg:mt-10">
